f17bdfec0cbc21cd34d5bf3dda6a8472865c6977
Corentin <>
2023-08-07 13:36:09 +0200

    constant variables

--- model.py
+++ model.py
@@ -4,28 +4,33 @@ from stable_baselines3 import DQN
 from stable_baselines3.common.evaluation import evaluate_policy
 from stable_baselines3.common.monitor import Monitor
 
+MAX_STEPS = 200
+SEED = 42
+N_EVAL_EPISODES = 10
+LEARNING_TIMESTEPS = 30000
+
 # Register our custom environment
 gym.register(
     id="JumboEnv-v0",
     entry_point="jumbo_gym:JumboEnv",
-    max_episode_steps=200,
+    max_episode_steps=MAX_STEPS,
 )
 
-np.random.seed(42)
+np.random.seed(SEED)
 # Create the environment
 env = gym.make("JumboEnv-v0")
 env = Monitor(env)
 
 # Train DQN Model
 model = DQN("MlpPolicy", env, verbose=1)
-model.learn(total_timesteps=30000)
+model.learn(total_timesteps=LEARNING_TIMESTEPS)
 model.save("dqn_matrix_env")
 
 # Evaluate the models without PyGame render, only the reward
-mean_reward_epsilon_greedy, _ = evaluate_policy(model, env, n_eval_episodes=10)
-print("Mean reward: ", mean_reward_epsilon_greedy)
+mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=N_EVAL_EPISODES)
+print("Mean reward: ", mean_reward)
 
 
 # Evaluate the model visually with PyGame render
 env.set_render_mode("human")
-mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=10)
+mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=N_EVAL_EPISODES)
